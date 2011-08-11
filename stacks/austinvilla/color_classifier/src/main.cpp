d11c1f526b455b48a9aa15a3e1fa74cb3d629e70
piyushk <piyushk@aea2af56-0017-d0a0-7fdb-f656f34a69f4>
2011-08-11 17:07:26 +0000

    included doxygen header for main.cpp

--- stacks/austinvilla/color_classifier/src/main.cpp
+++ stacks/austinvilla/color_classifier/src/main.cpp
@@ -1,3 +1,15 @@
+/**
+ * \file  main.cpp
+ * \brief Runs the color classifier
+ *
+ * \author  Piyush Khandelwal (piyushk), piyushk@cs.utexas.edu
+ * Copyright (C) 2011, The University of Texas at Austin, Piyush Khandelwal
+ *
+ * License: Modified BSD License
+ *
+ * $ Id: 08/11/2011 11:19:31 AM piyushk $
+ */
+
 #include <QtGui>
 #include <QApplication>
 #include <color_classifier/main_window.hpp>
