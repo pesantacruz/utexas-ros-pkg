9b8b61254fc1a8472b60a344b78f2ada4a5093c9
piyushk <piyushk@aea2af56-0017-d0a0-7fdb-f656f34a69f4>
2011-10-06 03:36:06 +0000

    initial draft of camera calibration script

--- branches/experimental/ar_nav/ar_localization/src/nodes/calibrate.cc
+++ branches/experimental/ar_nav/ar_localization/src/nodes/calibrate.cc
@@ -29,15 +29,93 @@
 
 
 #include <ros/ros.h>
+#include <tf/tf.h>
+#include <tf/transform_listener.h>
+#include <ros/package.h>
+
+#include <boost/foreach.hpp>
+
+#include <ar_nav_common/PatternParser.h>
 
 #define NODE "ar_localization_calibrate"
 
 namespace {
+  std::string patternFile;
+  std::string cameraBaseFrame;
+  std::string cameraId;
+}
 
-   
-
+void getParams(ros::NodeHandle& nh) {
+  nh.getParam("patternFile", patternFile);
+  nh.getParam("cameraBaseFrame", cameraBaseFrame);
+  nh.getParam("cameraId", cameraId);
 }
 
 int main (int argc, char **argv) {
 
+  ros::init(argc, argv, NODE);
+  ros::NodeHandle nh;
+  getParams(nh);
+
+  // Get information about the patterns
+  std::vector<ar_nav::Pattern> patterns;
+  ar_nav::readPatternFile(patternFile, patterns);
+
+  tf::TransformListener listener(nh);
+  tf::Vector3 averageTranslation(0,0,0);
+  tf::Quaternion averageRotation(0,0,0,0);
+  int numTransformations = 0;
+
+  // Get transformations for every marker
+  BOOST_FOREACH(const ar_nav::Pattern& pattern, patterns) {
+
+    // Get transform from /marker to /camera
+    tf::StampedTransform transformMarkerToCam;
+    bool transformAvailable = true;
+    try {
+      listener.lookupTransform(cameraBaseFrame, pattern.name,  
+                               ros::Time(0), transformMarkerToCam);
+    } catch (tf::TransformException ex) {
+      transformAvailable = false;
+      ROS_ERROR("Error getting transformation for %s: %s", pattern.name.c_str(), ex.what());
+    }
+
+    if (!transformAvailable) {
+      continue;
+    } 
+
+    // Compute transform of /map to /marker
+    tf::Quaternion noRotation(0, 0, 0, 0);
+    tf::Vector3 markerPosition(pattern.location.x, pattern.location.y, pattern.location.z);
+    tf::Transform transformWorldToMarker(noRotation, markerPosition);
+
+    // Compute transformation from /map to /camera
+    tf::Transform transformWorldToCam(tf::Quaternion(0,0,0,0), tf::Vector3(0,0,0));
+    transformWorldToCam.mult(transformWorldToMarker, transformWorldToCam);
+
+    averageTranslation += transformWorldToCam.getOrigin();
+    averageRotation += transformWorldToCam.getRotation();  
+    numTransformations++;  
+
+  }
+
+  if (numTransformations == 0) {
+    ROS_ERROR("Unable to get any viable transforms from markers");
+    return 0;
+  }
+
+  averageTranslation *= 1.0 / numTransformations;
+  averageRotation *= 1.0 / numTransformations;
+
+  std::ofstream fout((ros::package::getPath("ar_localization") + "/launch/" + cameraId).c_str());
+
+  fout << "<launch>" << std::endl;
+  fout << "  <node pkg=\"tf\" type=\"static_transform_publisher\" name=\"link_" << cameraId << "_broadcaster\""
+       << "args=\"" << averageTranslation.x() << " " << averageTranslation.y() << " " << averageTranslation.z()
+       << " " << averageRotation.getX() << " " << averageRotation.getY() << " " << averageRotation.getZ() << " "
+       << averageRotation.getW() << " map " << cameraBaseFrame << " 100\" />" << std::endl;
+  fout << "</launch>" << std::endl;
+  fout.close();
+
+  return 0;
 }
