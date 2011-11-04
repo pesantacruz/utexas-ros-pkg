59397d75f0d9e514ebb26e67625622d4cbaec568
piyushk@gmail.com <piyushk@gmail.com@aea2af56-0017-d0a0-7fdb-f656f34a69f4>
2011-11-04 20:33:39 +0000

    the get address now runs in a loop - good while booting up as sometimes the network interace does not connect in time

--- branches/experimental/scripts/get_addr.py
+++ branches/experimental/scripts/get_addr.py
@@ -2,16 +2,20 @@
 
 import sys
 import netifaces
+import time
 
-for inf in netifaces.interfaces():
-  if inf.startswith('ath0'):
-    addrs = netifaces.ifaddresses(inf)
-    if not netifaces.AF_INET in addrs:
-      continue
-    else:
-      print addrs[netifaces.AF_INET][0]['addr']
-      break
-else:
-  print >> sys.stderr, "failed to determine IP address"
-  print "127.0.0.1" #bind to loopback for now
-  sys.exit(1)
+counter = 0
+while counter < 5:
+  for inf in netifaces.interfaces():
+    if inf.startswith('ath0'):
+      addrs = netifaces.ifaddresses(inf)
+      if not netifaces.AF_INET in addrs:
+        continue
+      else:
+        print addrs[netifaces.AF_INET][0]['addr']
+        sys.exit(0)
+  time.sleep(2)
+
+print >> sys.stderr, "failed to determine IP address"
+print "127.0.0.1" #bind to loopback for now
+sys.exit(1)
