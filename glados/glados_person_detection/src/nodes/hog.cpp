d5581bf4cc6bef7cd57266e919db5b4582e7613b
piyushk <piyushk@aea2af56-0017-d0a0-7fdb-f656f34a69f4>
2011-11-18 22:14:09 +0000

    now produces person detection pose in map frame, also reorganized code structure

--- glados/glados_person_detection/src/nodes/hog.cpp
+++ glados/glados_person_detection/src/nodes/hog.cpp
@@ -1,17 +1,17 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
-#include <geometry_msgs/Point.h>
-#include "Detector.h"
+#include <geometry_msgs/PoseStamped.h>
+#include <glados_person_detection/Detector.h>
 
 int main(int argc, char **argv) {
   ros::init(argc,argv,"glados_person_detection");
   ros::NodeHandle nh;
   image_transport::ImageTransport it(nh);
-  ros::Publisher pub = nh.advertise<geometry_msgs::Point>("glados_person_detection/person",5);
+  ros::Publisher pub = nh.advertise<geometry_msgs::PoseStamped>("glados_person_detection/person",5);
   bool doDisplay;
   ros::param::param("~display",doDisplay,true);
   Detector detector(&pub,doDisplay);
-  image_transport::Subscriber sub = it.subscribe("glados_person_detection/image_raw",1,&Detector::detect,&detector);
+  image_transport::CameraSubscriber sub = it.subscribeCamera("glados_person_detection/image_raw",1,&Detector::detect,&detector);
   ros::spin();
   return 0;
 }
