224c9c2e466540d701197a2e5f85a04859f6a5ab
Wolfgang.Yang <Wolfgang.Yang@aea2af56-0017-d0a0-7fdb-f656f34a69f4>
2012-02-16 01:51:05 +0000

    checking in ccalc_driver

---
+++ branches/experimental/ccalc/ccalc_driver/planner.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('ccalc_driver')
+import rospy
+import pexpect
+import subprocess
+import sys
+import os
+
+swipl_param=rospy.get_param("ccalc_driver/swi_prolog_location")
+ccalc_param=rospy.get_param("ccalc_driver/ccalc_location")
+print "Launch CCalc with file "+sys.argv[1]
+launch= swipl_param+" -f "+ccalc_param
+ccalc = pexpect.spawn(launch)
+ccalc.expect('\?\-')
+ccalc.sendline('set(solver,grasp).')
+ccalc.expect('\?\-')
+loadfile="loadf \'"+sys.argv[1]+"\'.\n"
+ccalc.sendline(loadfile)
+ccalc.expect('\?\-')
+ccalc.sendline('query 0.\n')
+ccalc.expect('\?\-')
+ccalc.sendline('halt.')
+ccalc.interact()
+
+
+
+
+
