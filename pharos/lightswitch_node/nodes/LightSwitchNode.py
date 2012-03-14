4c6682247dd9bd17f4a454c7384dc6639fd6c2e8
liangfok@gmail.com <liangfok@gmail.com@aea2af56-0017-d0a0-7fdb-f656f34a69f4>
2012-03-14 16:04:57 +0000

    Modified LightSwitchNode to publish AmbientLight messages to topic lightLevel

--- pharos/lightswitch_node/nodes/LightSwitchNode.py
+++ pharos/lightswitch_node/nodes/LightSwitchNode.py
@@ -2,6 +2,7 @@
 import roslib; roslib.load_manifest('lightswitch_node')
 import rospy
 from lightswitch_node.msg import LightSwitchCmd
+from lightswitch_node.msg import AmbientLight
 import serial, time, struct
 from threading import Thread
 
@@ -17,10 +18,12 @@ class SerialMonitor(Thread): # SerialMonitor extends Thread
 
   Parameters:
    - ser: The serial port object
+   - pub: The object through which LightLevel messages may be published
   '''
-  def __init__(self, ser):
+  def __init__(self, ser, pub):
     Thread.__init__(self, name="SerialMonitor") # Call superclass constructor
-    self.ser = ser;
+    self.ser = ser; 
+    self.seqno = 0;
 
   def run(self):
     rospy.loginfo(rospy.get_name() + " SerialMonitor: Thread starting.")
@@ -28,16 +31,28 @@ class SerialMonitor(Thread): # SerialMonitor extends Thread
       if (ser.inWaiting() > 0):
         lightLevel = ser.read(1)
 	lightLevel = struct.unpack("B", lightLevel) # convert from string to tuple (an immutable sequence type)
-	rospy.loginfo(rospy.get_name() + " SerialMonitor: light level = %i", lightLevel[0])
+	rospy.logdebug(rospy.get_name() + " SerialMonitor: light level = %i", lightLevel[0])
+        
+        # Create an AmbientLight message
+        msg = AmbientLight()
+        msg.header.seq = self.seqno
+        msg.header.stamp = rospy.get_rostime()
+        msg.header.frame_id = "light_sensor"
+        msg.lightLevel = lightLevel[0]
+
+        self.seqno += 1
+        
+        # Publish the AmbientLight message
+        pub.publish(msg)
       time.sleep(0.05)  # cycle at 20Hz
 
 def callback(cmd):
-    rospy.loginfo(rospy.get_name() + " - Received command %i", cmd.cmd)
+    rospy.loginfo(rospy.get_name() + " - Received light switch command %i", cmd.cmd)
     data = [cmd.cmd]
     # Format chars: http://docs.python.org/library/struct.html#format-characters
     bytes = struct.pack("<b", *data)
     numTX = ser.write(bytes)
-    rospy.loginfo(rospy.get_name() + " - Sent command (%i bytes)", numTX)
+    #rospy.loginfo(rospy.get_name() + " - Sent command (%i bytes)", numTX)
 			
 if __name__ == '__main__':
     rospy.init_node('lightswitch_node')
@@ -60,9 +75,10 @@ if __name__ == '__main__':
 
     ser.flushInput()
 
-    # Create and start serial monitor thread.
+    # Create and start a serial monitor thread.
     # This is for receiving light level information.
-    smThread = SerialMonitor(ser)
+    pub = rospy.Publisher('lightLevel', AmbientLight)
+    smThread = SerialMonitor(ser, pub)
     smThread.start()
 
     rospy.Subscriber("lightswitch_cmd", LightSwitchCmd, callback)
