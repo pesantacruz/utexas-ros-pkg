e7a7504a4a2182b8a96a9b5a06d3b1e90b6834ac
piyushk <piyushk@aea2af56-0017-d0a0-7fdb-f656f34a69f4>
2012-04-26 22:58:27 +0000

    fixed a small bug in the sketch

--- pharos/pharos_gyro/arduino/GyroRead/GyroRead.ino
+++ pharos/pharos_gyro/arduino/GyroRead/GyroRead.ino
@@ -10,9 +10,9 @@ int sensorPin1 = A1;    // select the input pin for the potentiometer
 int sensorPin2 = A2;    // select the input pin for the potentiometer
 
 int ledPin = 13;      // select the pin for the LED
+int sensorValue0 = 0;  // variable to store the value coming from the sensor
 int sensorValue1 = 0;  // variable to store the value coming from the sensor
 int sensorValue2 = 0;  // variable to store the value coming from the sensor
-int sensorValue3 = 0;  // variable to store the value coming from the sensor
 
 int ledState = 0;
 
@@ -24,15 +24,12 @@ void setup() {
 
 void loop() {
   // read the value from the sensor:
-  sensorValue1 = analogRead(sensorPin0);    
-  sensorValue2 = analogRead(sensorPin1);    
-  sensorValue3 = analogRead(sensorPin2); 
+  sensorValue0 = analogRead(sensorPin0); // Yaw
+  sensorValue1 = analogRead(sensorPin1); // Pitch  
+  sensorValue2 = analogRead(sensorPin2); // Reference level
   
-  float val = (float) sensorValue1 / (float) sensorValue2;
-  
+  float val = (float) sensorValue0 / (float) sensorValue2;
   Serial.println(val);
-  //Serial.print(", ");
-  //Serial.println(sensorValue3);
   
   if (ledState) {
     // turn the ledPin on
