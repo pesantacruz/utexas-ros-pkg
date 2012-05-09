1dec5185ac60f2122d7b9f5142bef149be72aa9b
piyushk <piyushk@aea2af56-0017-d0a0-7fdb-f656f34a69f4>
2012-05-09 21:01:40 +0000

    added script for setting ROS_IP using IP from network interfaces

---
+++ stacks/pharos/pharos_multimaster/scripts/get_addr.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+import netifaces
+
+if len(sys.argv) > 1:
+  interface = sys.argv[1]
+else:
+  interface = 'wlan0'
+
+for inf in netifaces.interfaces():
+  if inf.startswith(interface):
+    addrs = netifaces.ifaddresses(inf)
+    if not netifaces.AF_INET in addrs:
+      continue
+    else:
+      print addrs[netifaces.AF_INET][0]['addr']
+      sys.exit(0)
+
+print >> sys.stderr, "failed to determine IP address"
+print "localhost" #bind to loopback for now
+sys.exit(1)
