7a769dc55d2961a9edfc581d4703d2f4b0f4976f
piyushk <piyushk@aea2af56-0017-d0a0-7fdb-f656f34a69f4>
2012-07-26 23:35:16 +0000

    finished implementation with testing and a bit of cleanup. should be good enough to develop other applications on.

--- stacks/april/april_tags_node/src/main/java/april/ros/TagPublisher.java
+++ stacks/april/april_tags_node/src/main/java/april/ros/TagPublisher.java
@@ -30,7 +30,7 @@ import java.lang.Math;
 
 public class TagPublisher extends AbstractNodeMain {
 
-  private boolean focal_length_available = false;
+  private boolean focalLengthAvailable = false;
   
   private String tf_str = "april.tag.Tag36h11";
   private TagFamily tf;
@@ -40,8 +40,17 @@ public class TagPublisher extends AbstractNodeMain {
   private double focalLengthX;
   private double focalLengthY;
 
-  public static BufferedImage messageToBufferedImage(sensor_msgs.Image message) {
-    BufferedImage im = new BufferedImage(message.getWidth(), message.getHeight(), BufferedImage.TYPE_INT_RGB);
+  /**
+   * Simple but inefficient conversion of Image message to Buffered Image.
+   */
+  public static BufferedImage messageToBufferedImage(
+      sensor_msgs.Image message) {
+
+    BufferedImage im = 
+        new BufferedImage(message.getWidth(), 
+                          message.getHeight(), 
+                          BufferedImage.TYPE_INT_RGB);
+
     for (int x = 0; x < message.getWidth(); x++) {
       for (int y = 0; y < message.getHeight(); y++) {
         byte red = 
@@ -56,90 +65,94 @@ public class TagPublisher extends AbstractNodeMain {
         im.setRGB(x, y, rgb);
       }
     }
+
     return im;
   }
 
-  public static BufferedImage messageToBufferedImageFast(sensor_msgs.Image message) {
+  /** 
+   * Efficient but channel flipped conversion of Image message to Buffered
+   * Image. The image message data stores data in 3 bytes as RGBRGBR...., 
+   * which is not supported directly by BufferedImage. The only 3 byte 
+   * representation supported by BufferedImage is 3BYTE_BGR. Since the tags are
+   * unaffected by color, we flip the R and B channels in this efficient copy.
+   */
+  public static BufferedImage messageToBufferedImageFast(
+      sensor_msgs.Image message) {
+
     int width = message.getWidth();
     int height = message.getHeight();
-    DataBufferByte dataBuffer = new DataBufferByte(message.getData().array(), message.getData().array().length);
-    PixelInterleavedSampleModel sampleModel
-      = new PixelInterleavedSampleModel(DataBuffer.TYPE_BYTE, width, height, 3, 3*width, new int[] {2,1,0});        
+
+    DataBufferByte dataBuffer = 
+      new DataBufferByte(message.getData().array(), 
+                         message.getData().array().length);
+    PixelInterleavedSampleModel sampleModel = 
+      new PixelInterleavedSampleModel(
+          DataBuffer.TYPE_BYTE, width, height, 3, 3*width, new int[] {2,1,0}); 
+    WritableRaster raster = 
+      Raster.createWritableRaster(sampleModel, dataBuffer, new Point(0,0));
+
     ColorSpace cs = ColorSpace.getInstance(ColorSpace.CS_sRGB);
-    ColorModel colourModel = new ComponentColorModel(cs, false, false, Transparency.OPAQUE, DataBuffer.TYPE_BYTE);
-    WritableRaster raster = Raster.createWritableRaster(sampleModel, dataBuffer, new Point(0,0));
+    ColorModel colourModel = new ComponentColorModel(
+        cs, false, false, Transparency.OPAQUE, DataBuffer.TYPE_BYTE);
+
     BufferedImage im = new BufferedImage(colourModel, raster, false, null);
+
     return im;
   }
 
-  /* http://www.euclideanspace.com/maths/geometry/affine/conversions/matrixToQuaternion/index.htm */
-  public static void projectionMatrixToCenterOfRotation(double M[][], geometry_msgs.Point point) {
-    
-    Matrix m = new Matrix(4,4);
-    m.set(0,0,M);
-    double m_det = m.det(); 
-
-    Matrix M1 = new Matrix(3,3);
-
-    M1.set(0, 0, (M[1][1] - 1) * M[2][2] - M[1][2] * M[2][1]);
-    M1.set(0, 1, M[0][2] * M[2][1] - M[0][1] * (M[2][2] - 1));
-    M1.set(0, 2, M[0][1] * M[1][2] - M[0][2] * (M[1][1] - 1));
-
-    M1.set(1, 0, M[1][2] * M[2][0] - M[1][0] * (M[2][2] - 1));
-    M1.set(1, 1, M[0][0] * (M[2][2] - 1) - M[0][2] * M[2][0]);
-    M1.set(1, 2, M[0][2] * M[1][0] - (M[0][0] - 1) * M[1][2]);
-
-    M1.set(2, 0, M[1][0] * M[2][1] - (M[1][1] - 1) * M[2][0]);
-    M1.set(2, 1, M[0][1] * M[2][0] - (M[0][0] - 1) * M[2][1]);
-    M1.set(2, 2, M[0][0] * (M[1][1] - 1) - M[0][1] * M[1][0]);
-
-    Matrix M2 = new Matrix(3,1);
-    M2.set(0, 0, M[0][3]);
-    M2.set(1, 0, M[1][3]);
-    M2.set(2, 0, M[2][3]);
-
-    Matrix C = M1.times(M2);
-    C = C.times(1.0/m_det);
-
-    point.setX(C.get(0,0));
-    point.setY(C.get(1,0));
-    point.setZ(C.get(2,0));
-
+  /** Compute tag location in the standard ROS frame convention for optical
+   * frames. Tag location is computed from the translational component of
+   * the projection matrix. 
+   * To get more details about the frame convention, see REP 103 
+   * (http://www.ros.org/reps/rep-0103.html)
+   */
+  public static void projectionMatrixToCenterOfRotation(
+      double M[][], geometry_msgs.Point point) {
     point.setX(M[0][3]);
-    point.setY(M[1][3]);
-    point.setZ(M[2][3]);
+    point.setY(-M[1][3]);
+    point.setZ(-M[2][3]);
   }
 
-/* http://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToQuaternion/ */
-  public static void projectionMatrixToQuaternion(double M[][], geometry_msgs.Quaternion q) {
+  /** Compute tag orientation as a Quaternion. Tag orientation is computed from
+   * the rotational component of the projection matrix. The methodology was
+   * taken from http://en.wikipedia.org/wiki/Rotation_matrix#Quaternion 
+   */
+  public static void projectionMatrixToQuaternion(
+      double M[][], geometry_msgs.Quaternion q) {
 
     double qx, qy, qz, qw;
 
-    double tr = M[0][0] + M[1][1] + M[2][2];
-    if (tr > 0) { 
-      double S = Math.sqrt(tr+1.0) * 2; // S=4*qw 
-      qw = 0.25 * S;
-      qx = (M[2][1] - M[1][2]) / S;
-      qy = (M[0][2] - M[2][0]) / S; 
-      qz = (M[1][0] - M[0][1]) / S; 
-    } else if ((M[0][0] > M[1][1])&(M[0][0] > M[2][2])) { 
-      double S = Math.sqrt(1.0 + M[0][0] - M[1][1] - M[2][2]) * 2; // S=4*qx 
-      qw = (M[2][1] - M[1][2]) / S;
-      qx = 0.25 * S;
-      qy = (M[0][1] + M[1][0]) / S; 
-      qz = (M[0][2] + M[2][0]) / S; 
-    } else if (M[1][1] > M[2][2]) { 
-      double S = Math.sqrt(1.0 + M[1][1] - M[0][0] - M[2][2]) * 2; // S=4*qy
-      qw = (M[0][2] - M[2][0]) / S;
-      qx = (M[0][1] + M[1][0]) / S; 
-      qy = 0.25 * S;
-      qz = (M[1][2] + M[2][1]) / S; 
-    } else { 
-      double S = Math.sqrt(1.0 + M[2][2] - M[0][0] - M[1][1]) * 2; // S=4*qz
-      qw = (M[1][0] - M[0][1]) / S;
-      qx = (M[0][2] + M[2][0]) / S;
-      qy = (M[1][2] + M[2][1]) / S;
-      qz = 0.25 * S;
+    double t = M[0][0] + M[1][1] + M[2][2]; 
+    if (t > 0) {
+      double r = Math.sqrt(1+t);
+      double s = 0.5f / r;
+      qw = 0.5 * r;
+      qx = (M[2][1] - M[1][2]) * s;
+      qy = (M[0][2] - M[2][0]) * s;
+      qz = (M[1][0] - M[0][1]) * s;
+    } else {
+      if (M[0][0] > M[1][1] && M[0][0] > M[2][2]) {
+        double r = Math.sqrt(1.0f + M[0][0] - M[1][1] - M[2][2]);
+        double s = 0.5f / r;
+        qw = (M[2][1] - M[1][2]) * s;
+        qx = 0.5f * r;
+        qy = (M[0][1] + M[1][0]) * s;
+        qz = (M[2][0] + M[0][2]) * s;
+      } else if (M[1][1] > M[2][2]) {
+        double r = Math.sqrt(1.0f + M[1][1] - M[0][0] - M[2][2]);
+        double s = 0.5f / r;
+        qw = (M[0][2] - M[2][0]) * s;
+        qx = (M[0][1] + M[1][0]) * s;
+        qy = 0.5f * r;
+        qz = (M[1][2] + M[2][1]) * s;
+      } else {
+        double r = Math.sqrt(1.0f + M[2][2] - M[0][0] - M[1][1]);
+        double s = 0.5f / r;
+        qw = (M[1][0] - M[0][1]) * s;
+        qx = (M[0][2] + M[2][0]) * s;
+        qy = (M[1][2] + M[2][1]) * s;
+        qz = 0.5f * r;
+      }
     }
 
     q.setX(qx);
@@ -148,7 +161,10 @@ public class TagPublisher extends AbstractNodeMain {
     q.setW(qw);
   }
 
-  public static void projectionMatrixToPoseMessage(double M[][], geometry_msgs.Pose message) {
+  /** Compute the entire tag pose from the projection matrix
+   */
+  public static void projectionMatrixToPoseMessage(
+      double M[][], geometry_msgs.Pose message) {
     TagPublisher.projectionMatrixToCenterOfRotation(M, message.getPosition());
     TagPublisher.projectionMatrixToQuaternion(M, message.getOrientation());
   }
@@ -159,7 +175,7 @@ public class TagPublisher extends AbstractNodeMain {
   }
 
   @Override
-  public void onStart(ConnectedNode node) {
+  public void onStart(final ConnectedNode node) {
 
     // Get logger to publish ROS log messages
     final Log log = node.getLog();
@@ -195,17 +211,17 @@ public class TagPublisher extends AbstractNodeMain {
 
     // Tag pose estimation parameters
     this.tagSize = params.getDouble("~tag_size", 0.095);
-    boolean use_camera_info = params.getBoolean("~use_camera_info", false);
+    boolean useCameraInfo = params.getBoolean("~useCameraInfo", false);
     this.focalLengthX = params.getDouble("~focal_length_x", 485.6);
     this.focalLengthY = params.getDouble("~focal_length_y", this.focalLengthX);
-    if (!use_camera_info) {
-      focal_length_available = true;
+    if (!useCameraInfo) {
+      focalLengthAvailable = true;
       log.info("Focal length values fx = " + focalLengthX + ", fy = " + focalLengthY);
     }
 
     // Create the publisher
-    final Publisher<geometry_msgs.PoseStamped> publisher = 
-      node.newPublisher("~pose", geometry_msgs.PoseStamped._TYPE);
+    final Publisher<april_msgs.TagPoseArray> publisher = 
+      node.newPublisher("~tags", april_msgs.TagPoseArray._TYPE);
 
     // Subscribe to the image message    
     Subscriber<sensor_msgs.Image> image_subscriber =
@@ -213,53 +229,105 @@ public class TagPublisher extends AbstractNodeMain {
     image_subscriber.addMessageListener(new MessageListener<sensor_msgs.Image>() {
       @Override
       public void onNewMessage(sensor_msgs.Image message) {
-        if (!focal_length_available) {
+        if (!focalLengthAvailable) {
           log.debug("Waiting for camera info message");
           return;
         }
         BufferedImage im = TagPublisher.messageToBufferedImageFast(message);
-        ArrayList<TagDetection> detections = detector.process(im, new double[] {im.getWidth()/2.0, im.getHeight()/2.0});
-        log.info("Num Detections: " + detections.size());
+        ArrayList<TagDetection> detections = 
+            detector.process(im, new double[] {im.getWidth()/2.0, im.getHeight()/2.0});
 
         // Publish information corresponding to every detection
+        april_msgs.TagPoseArray tagArray = publisher.newMessage();
+        tagArray.setHeader(message.getHeader());
+        java.util.List<april_msgs.TagPose> tags = tagArray.getTags();
+
+        // For each tag, add the pose to the tag list
         for (TagDetection d : detections) {
           double p0[] = d.interpolate(-1,-1);
           double p1[] = d.interpolate(1,-1);
           double p2[] = d.interpolate(1,1);
           double p3[] = d.interpolate(-1,1);
-          double M[][] = CameraUtil.homographyToPose(focalLengthX, focalLengthY, tagSize, d.homography);
+          double M[][] = 
+            CameraUtil.homographyToPose(focalLengthX, focalLengthY, tagSize, d.homography);
 
-          geometry_msgs.PoseStamped poseMsg = publisher.newMessage();
-          poseMsg.setHeader(message.getHeader());
-          TagPublisher.projectionMatrixToPoseMessage(M, poseMsg.getPose());
-          publisher.publish(poseMsg);
+          // Copy all information to a TagPose message
+          april_msgs.TagPose tag = 
+            node.getTopicMessageFactory().newFromType(april_msgs.TagPose._TYPE);
+
+          // Tag id and hamming distance error
+          tag.setId(d.id);
+          tag.setHammingDistance(d.hammingDistance);
+
+          // Image coordinates of Tag Detection
+          java.util.List<geometry_msgs.Point32> imageCoordinates = 
+            tag.getImageCoordinates();
+          // p0
+          geometry_msgs.Point32 pt0 = 
+            node.getTopicMessageFactory().newFromType(geometry_msgs.Point32._TYPE);
+          pt0.setX((float)p0[0]);
+          pt0.setY((float)p0[1]);
+          pt0.setZ(0);
+          imageCoordinates.add(pt0);
+          // p1
+          geometry_msgs.Point32 pt1 = 
+            node.getTopicMessageFactory().newFromType(geometry_msgs.Point32._TYPE);
+          pt1.setX((float)p1[0]);
+          pt1.setY((float)p1[1]);
+          pt1.setZ(0);
+          imageCoordinates.add(pt1);
+          // p2
+          geometry_msgs.Point32 pt2 = 
+            node.getTopicMessageFactory().newFromType(geometry_msgs.Point32._TYPE);
+          pt2.setX((float)p2[0]);
+          pt2.setY((float)p2[1]);
+          pt2.setZ(0);
+          imageCoordinates.add(pt2);
+          // p3
+          geometry_msgs.Point32 pt3 = 
+            node.getTopicMessageFactory().newFromType(geometry_msgs.Point32._TYPE);
+          pt3.setX((float)p3[0]);
+          pt3.setY((float)p3[1]);
+          pt3.setZ(0);
+          imageCoordinates.add(pt3);
+
+          // Pose of the the tag
+          TagPublisher.projectionMatrixToPoseMessage(M, tag.getPose());
+
+          // Add tag to the array
+          tags.add(tag);
         }
+        
+        // Finally publish all detected tags
+        publisher.publish(tagArray);
       }
-    });
+    }); /* end addMessageListener */
 
     // Subscribe to camera info message if necessary
-    if (use_camera_info) {
+    if (useCameraInfo) {
       Subscriber<sensor_msgs.CameraInfo> info_subscriber =
         node.newSubscriber("camera_info", sensor_msgs.CameraInfo._TYPE);
       info_subscriber.addMessageListener(new MessageListener<sensor_msgs.CameraInfo>() {
         @Override
         public void onNewMessage(sensor_msgs.CameraInfo message) {
-          if (!focal_length_available) {
+          if (!focalLengthAvailable) {
             log.info("Camera Info message received");
             double[] k = message.getK();
             if (k.length != 9) {
-              log.error("Camera info message has malformed or non-existent intrinsic camera matrix; unable to retrieve focal length. Using default values!");
+              log.error("Camera info message has malformed or non-existent " +
+                "intrinsic camera matrix; unable to retrieve focal length. " +
+                "Using default values!");
             } else {
               focalLengthX = k[0];
               focalLengthY = k[4];
               log.info("Focal length selected from camera info message");
             }
             log.info("Focal length values fx = " + focalLengthX + ", fy = " + focalLengthY);
-            focal_length_available = true;
+            focalLengthAvailable = true;
           }
         }
       });
-    } /* end use_camera_info */
+    } /* end useCameraInfo */
 
   }
 }
