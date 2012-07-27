40425be7844170ae4ff3645068d0760bbd9fc8af
jmunoz802 <jmunoz802@aea2af56-0017-d0a0-7fdb-f656f34a69f4>
2012-07-27 21:53:06 +0000

    code cleanup

--- branches/experimental/stacks/pharos_segbot/segbot_controller/src/main_gui.cpp
+++ branches/experimental/stacks/pharos_segbot/segbot_controller/src/main_gui.cpp
@@ -12,35 +12,33 @@ public:
 private:
     segbot_controller::QNode rosnode;
 };
+  MyWidget::MyWidget(QWidget *parent, int argc, char *argv[])
+  : QWidget(parent),rosnode(argc,argv)
+  {
+    QPushButton *quit = new QPushButton(tr("Quit"));
+    quit->setFont(QFont("Times", 18, QFont::Bold));
 
-    MyWidget::MyWidget(QWidget *parent, int argc, char *argv[])
-    : QWidget(parent),rosnode(argc,argv)
-    {
-        QPushButton *quit = new QPushButton(tr("Quit"));
-        quit->setFont(QFont("Times", 18, QFont::Bold));
+    QPushButton *stop = new QPushButton(tr("Stop"));
+    stop->setFont(QFont("Times", 18, QFont::Bold));
 
-        QPushButton *stop = new QPushButton(tr("Stop"));
-        stop->setFont(QFont("Times", 18, QFont::Bold));
+    QPushButton *cont = new QPushButton(tr("Continue"));
+    cont->setFont(QFont("Times", 18, QFont::Bold));
 
-        QPushButton *cont = new QPushButton(tr("Continue"));
-        cont->setFont(QFont("Times", 18, QFont::Bold));
-
-        connect(quit, SIGNAL(clicked()), qApp, SLOT(quit()));
-        QObject::connect(stop, SIGNAL(clicked()), &rosnode, SLOT(stop()));
-        QObject::connect(cont, SIGNAL(clicked()), &rosnode, SLOT(cont()));
+    connect(quit, SIGNAL(clicked()), qApp, SLOT(quit()));
+    QObject::connect(stop, SIGNAL(clicked()), &rosnode, SLOT(stop()));
+    QObject::connect(cont, SIGNAL(clicked()), &rosnode, SLOT(cont()));
                                                                                 
-       QVBoxLayout *layout = new QVBoxLayout;
-       layout->addWidget(stop);
-       layout->addWidget(cont);
-       layout->addWidget(quit);
-       setLayout(layout);
-    }
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->addWidget(stop);
+    layout->addWidget(cont);
+    layout->addWidget(quit);
+    setLayout(layout);
+  }
 
-
-    int main(int argc, char *argv[])
-    {
-       QApplication app(argc, argv);
-       MyWidget widget(NULL, argc, argv);
-       widget.show();
-       return app.exec();
-    }
+int main(int argc, char *argv[])
+{
+  QApplication app(argc, argv);
+  MyWidget widget(NULL, argc, argv);
+  widget.show();
+  return app.exec();
+}
