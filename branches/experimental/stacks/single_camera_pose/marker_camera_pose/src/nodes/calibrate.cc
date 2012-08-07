856499063432ad176c6a1073b360ba02e3b1e411
piyushk <piyushk@aea2af56-0017-d0a0-7fdb-f656f34a69f4>
2012-08-07 21:41:54 +0000

    cleaned up the calibrate node in marker_camera_pose, and moved the example patterns file to the marker package

--- branches/experimental/stacks/single_camera_pose/marker_camera_pose/src/nodes/calibrate.cc
+++ branches/experimental/stacks/single_camera_pose/marker_camera_pose/src/nodes/calibrate.cc
@@ -1,23 +1,10 @@
 /**
  * \file  calibrate.cc
  * \brief  Calculates position and orientation of any cameras using
- *         ARTags placed in known locations
+ *         fiducial markers placed in known locations
  *
- * The ar_multi node is laucnched automatically using roslaunch, and the
- * configuration is provided using a YAML file. ar_multi should already be
- * publishing this information on tf.
- *
- * A static transformation publisher should then be publishing the transform
- * between marker[i] -> world[i] (since the parent of the tree structure 
- * will be base_link on the camera, we require multiple worlds here)
- *
- * In this node we read the yaml file to figure out the number of markers
- * available, and read the corresponding transformations available from the
- * tf tree (i.e. world[i] -> camera_base_link).
- *
- * We average accross all of these to reduce error, and write out the
- * /world -> /camera_base_link transformation to file. The camera is
- * calibrated!
+ * It is assumed that the fiducial marker system is publishing the information
+ * on the tf tree for markers corresponding to the ones obtained from file
  *
  * \author  Piyush Khandelwal (piyushk), piyushk@cs.utexas.edu
  * Copyright (C) 2011, The University of Texas at Austin, Piyush Khandelwal
@@ -37,18 +24,20 @@
 
 #include <camera_pose/pattern.h>
 
-#define NODE "ar_localization_calibrate"
+#define NODE "extrinsic_camera_calibrate"
 
 namespace {
-  std::string patternFile;
-  std::string cameraBaseFrame;
-  std::string cameraId;
+  std::string pattern_file;
+  std::string camera_base_frame;
+  std::string launch_file_path;
+  std::string launch_camera_frame;
 }
 
 void getParams(ros::NodeHandle& nh) {
-  nh.getParam("patternFile", patternFile);
-  nh.getParam("cameraBaseFrame", cameraBaseFrame);
-  nh.getParam("cameraId", cameraId);
+  nh.getParam("pattern_file", pattern_file);
+  nh.getParam("camera_base_frame", camera_base_frame);
+  nh.getParam("launch_file_path", launch_file_path);
+  nh.getParam("launch_camera_frame", launch_camera_frame);
 }
 
 int main (int argc, char **argv) {
@@ -59,96 +48,112 @@ int main (int argc, char **argv) {
 
   // Get information about the patterns
   std::vector<camera_pose::Pattern> patterns;
-  camera_pose::readPatternFile(patternFile, patterns);
+  camera_pose::readPatternFile(pattern_file, patterns);
 
   //tf::TransformListener listener(nh);
   tf::TransformListener listener;
-  tf::Vector3 averageTranslation(0,0,0);
-  tf::Quaternion averageRotation(0,0,0,0);
-  int numTransformations = 0;
+  tf::Vector3 avg_translation(0,0,0);
+  tf::Quaternion avg_rotation(0,0,0,0);
+  int num_transformations = 0;
 
   // Get transformations for every marker
   BOOST_FOREACH(const camera_pose::Pattern& pattern, patterns) {
 
-    std::cout << pattern.name << std::endl;
+    ROS_DEBUG_STREAM("Evaulating marker: " << pattern.name);
 
-    // Get transform from /marker to /camera
-    tf::StampedTransform transformCamToMarker;
-    listener.waitForTransform(pattern.name, cameraBaseFrame, ros::Time(), ros::Duration(1.0)); 
+    // Get transform from /marker to /camera, - published by the fiducial 
+    // marker system
+    tf::StampedTransform transform_cam_to_marker;
+    listener.waitForTransform(
+        pattern.name, camera_base_frame, ros::Time(), ros::Duration(1.0)); 
     try {
-      listener.lookupTransform(cameraBaseFrame, pattern.name,
-                               ros::Time(), transformCamToMarker);
+      listener.lookupTransform(camera_base_frame, pattern.name,
+                               ros::Time(), transform_cam_to_marker);
     } catch (tf::TransformException ex) {
-      ROS_ERROR("Error getting transformation for %s: %s", pattern.name.c_str(), ex.what());
+      ROS_ERROR_STREAM("Error getting transformation for " << 
+          pattern.name.c_str() << ": " << ex.what());
       continue;
     }
 
-    tf::Quaternion q = transformCamToMarker.getRotation();
-    tf::Vector3 v = transformCamToMarker.getOrigin();
-    std::cout << "  CamToMarker" << std::endl;
-    std::cout << "  - Translation: [" << v.getX() << ", " << v.getY() << ", " << v.getZ() << "]" << std::endl;
-    std::cout << "  - Rotation: in Quaternion [" << q.getX() << ", " << q.getY() << ", "
-	  << q.getZ() << ", " << q.getW() << "]" << std::endl;
+    tf::Quaternion q = transform_cam_to_marker.getRotation();
+    tf::Vector3 v = transform_cam_to_marker.getOrigin();
+    ROS_DEBUG_STREAM("CamToMarker");
+    ROS_DEBUG_STREAM("  - Translation: [" << v.getX() << ", " 
+        << v.getY() << ", " << v.getZ() << "]");
+    ROS_DEBUG_STREAM("  - Rotation: in Quaternion [" << q.getX() << ", " 
+        << q.getY() << ", " << q.getZ() << ", " << q.getW() << "]");
 
     // Compute transform of /map to /marker
-    tf::Quaternion noRotation(0, 0, 0, 1);
-    tf::Vector3 markerPosition(-pattern.location.x, -pattern.location.y, -pattern.location.z);
-    tf::Transform transformMarkerToMap(noRotation, markerPosition);
+    tf::Quaternion no_rotation(0, 0, 0, 1);
+    tf::Vector3 marker_position(
+        -pattern.location.x, -pattern.location.y, -pattern.location.z);
+    tf::Transform transform_marker_to_map(no_rotation, marker_position);
 
-    q = transformMarkerToMap.getRotation();
-    v = transformMarkerToMap.getOrigin();
-    std::cout << "  MarkerToMap" << std::endl;
-    std::cout << "  - Translation: [" << v.getX() << ", " << v.getY() << ", " << v.getZ() << "]" << std::endl;
-    std::cout << "  - Rotation: in Quaternion [" << q.getX() << ", " << q.getY() << ", "
-	  << q.getZ() << ", " << q.getW() << "]" << std::endl;
+    q = transform_marker_to_map.getRotation();
+    v = transform_marker_to_map.getOrigin();
+    ROS_DEBUG_STREAM("  MarkerToMap");
+    ROS_DEBUG_STREAM("  - Translation: [" << v.getX() << ", " 
+        << v.getY() << ", " << v.getZ() << "]");
+    ROS_DEBUG_STREAM("  - Rotation: in Quaternion [" << q.getX() << ", " 
+        << q.getY() << ", " << q.getZ() << ", " << q.getW() << "]");
 
     // Compute transformation from /map to /camera
-    tf::Transform transformCamToMap(tf::Quaternion(0,0,0,1), tf::Vector3(0,0,0));
-    transformCamToMap.mult(transformCamToMarker, transformMarkerToMap);
-    q = transformCamToMap.getRotation();
-    v = transformCamToMap.getOrigin();
-    std::cout << "  CamToMap" << std::endl;
-    std::cout << "  - Translation: [" << v.getX() << ", " << v.getY() << ", " << v.getZ() << "]" << std::endl;
-    std::cout << "  - Rotation: in Quaternion [" << q.getX() << ", " << q.getY() << ", "
-	  << q.getZ() << ", " << q.getW() << "]" << std::endl;
+    tf::Transform transform_cam_to_map(
+        tf::Quaternion(0,0,0,1), tf::Vector3(0,0,0));
+    transform_cam_to_map.mult(transform_cam_to_marker, transform_marker_to_map);
+    q = transform_cam_to_map.getRotation();
+    v = transform_cam_to_map.getOrigin();
+    ROS_DEBUG_STREAM("  CamToMap");
+    ROS_DEBUG_STREAM("  - Translation: [" << v.getX() << ", " 
+        << v.getY() << ", " << v.getZ() << "]");
+    ROS_DEBUG_STREAM("  - Rotation: in Quaternion [" << q.getX() << ", " 
+        << q.getY() << ", " << q.getZ() << ", " << q.getW() << "]");
 
-    tf::Transform transformMapToCam = transformCamToMap.inverse();
-    q = transformMapToCam.getRotation();
-    v = transformMapToCam.getOrigin();
-    std::cout << "  MapToCam" << std::endl;
-    std::cout << "  - Translation: [" << v.getX() << ", " << v.getY() << ", " << v.getZ() << "]" << std::endl;
-    std::cout << "  - Rotation: in Quaternion [" << q.getX() << ", " << q.getY() << ", "
-	  << q.getZ() << ", " << q.getW() << "]" << std::endl;
+    tf::Transform transform_map_to_cam = transform_cam_to_map.inverse();
+    q = transform_map_to_cam.getRotation();
+    v = transform_map_to_cam.getOrigin();
+    ROS_DEBUG_STREAM("  MapToCam");
+    ROS_DEBUG_STREAM("  - Translation: [" << v.getX() << ", " 
+        << v.getY() << ", " << v.getZ() << "]");
+    ROS_DEBUG_STREAM("  - Rotation: in Quaternion [" << q.getX() << ", " 
+        << q.getY() << ", " << q.getZ() << ", " << q.getW() << "]");
 
-    numTransformations++;
+    num_transformations++;
 
-    if (numTransformations == 1) {
-      averageTranslation = transformMapToCam.getOrigin();
-      averageRotation = transformMapToCam.getRotation();  
+    if (num_transformations == 1) {
+      avg_translation = transform_map_to_cam.getOrigin();
+      avg_rotation = transform_map_to_cam.getRotation();  
     } else {
-      averageTranslation += transformMapToCam.getOrigin();
-      averageRotation += transformMapToCam.getRotation();  
+      avg_translation += transform_map_to_cam.getOrigin();
+      avg_rotation += transform_map_to_cam.getRotation();  
     }
 
   }
 
-  if (numTransformations == 0) {
+  if (num_transformations == 0) {
     ROS_ERROR("Unable to get any viable transforms from markers");
     return 0;
   }
 
-  averageTranslation *= 1.0 / numTransformations;
-  averageRotation *= 1.0 / numTransformations;
+  avg_translation *= 1.0 / num_transformations;
+  avg_rotation *= 1.0 / num_transformations;
 
-  // std::ofstream fout((ros::package::getPath("ar_localization") + "/launch/" + cameraId + "-tf.launch").c_str());
-
-  // fout << "<launch>" << std::endl;
-  // fout << "  <node pkg=\"tf\" type=\"static_transform_publisher\" name=\"link_" << cameraId << "_broadcaster\" "
-  //      << "args=\"" << averageTranslation.x() << " " << averageTranslation.y() << " " << averageTranslation.z()
-  //      << " " << averageRotation.getX() << " " << averageRotation.getY() << " " << averageRotation.getZ() << " "
-  //      << averageRotation.getW() << " map " << cameraBaseFrame << " 100\" />" << std::endl;
-  // fout << "</launch>" << std::endl;
-  // fout.close();
+  // Write compute transformation as a launch file
+  if (!launch_file_path.empty()) {
+    ROS_INFO_STREAM(" Writing camera launch file: " << launch_file_path);
+    // Write to file if provided
+    std::ofstream fout(launch_file_path.c_str());
+    fout << "<launch>" << std::endl;
+    fout << "  <node pkg=\"tf\" type=\"static_transform_publisher\"\n"
+         << "        name=\"link_" << launch_camera_frame << "_broadcaster\"\n"
+         << "        args=\"" << avg_translation.x() << " " 
+         << avg_translation.y() << " " << avg_translation.z() << " " 
+         << avg_rotation.getX() << " " << avg_rotation.getY() << " " 
+         << avg_rotation.getZ() << " " << avg_rotation.getW() << " /map /" 
+         << launch_camera_frame << " 100\" />" << std::endl;
+    fout << "</launch>" << std::endl;
+    fout.close();
+  }
 
   return 0;
 }
