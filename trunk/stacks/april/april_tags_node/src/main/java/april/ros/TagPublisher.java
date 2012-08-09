eba1311971f436aa28c52e5df2dbd3089073c3a6
piyushk <piyushk@aea2af56-0017-d0a0-7fdb-f656f34a69f4>
2012-08-09 18:58:26 +0000

    fixed licensing information for this stack, since it has now been moved to trunk

--- trunk/stacks/april/april_tags_node/src/main/java/april/ros/TagPublisher.java
+++ trunk/stacks/april/april_tags_node/src/main/java/april/ros/TagPublisher.java
@@ -1,3 +1,35 @@
+/**
+ * \file  TagPublisher.java
+ * \brief ROS Wrapper for the AprilTag toolkit
+ *
+ * \author Piyush Khandelwal
+ *
+ * Copyright (c) 2012, UT Austin
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the organization nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS".
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL UT AUSTIN BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **/
+
 package april.ros;
 
 import org.apache.commons.logging.Log;
@@ -296,8 +328,10 @@ public class TagPublisher extends AbstractNodeMain {
     ParameterTree params = node.getParameterTree();
 
     // Create detector object
-    final String tagFamilyStr = params.getString("~tag_family", "april.tag.Tag36h11"); 
-    final TagFamily tagFamily = (TagFamily) ReflectUtil.createObject(tagFamilyStr);
+    final String tagFamilyStr = 
+      params.getString("~tag_family", "april.tag.Tag36h11"); 
+    final TagFamily tagFamily = 
+      (TagFamily) ReflectUtil.createObject(tagFamilyStr);
     detector = new TagDetector(tagFamily);
 
     // Tag Detector Parameters
@@ -340,7 +374,8 @@ public class TagPublisher extends AbstractNodeMain {
     // ROS specific parameters
     final boolean publishVisualization = 
         params.getBoolean("~publish_visualization", true);
-    final double tagVisMagnification = params.getDouble("~tag_vis_magnification", 1);
+    final double tagVisMagnification = 
+      params.getDouble("~tag_vis_magnification", 1);
     final boolean broadcastTf = 
         params.getBoolean("~broadcast_tf", true);
 
@@ -416,7 +451,8 @@ public class TagPublisher extends AbstractNodeMain {
                 tagVisMagnification, tagSize, node.getTopicMessageFactory());
             markers.add(marker);
 
-            visualization_msgs.Marker markerText = node.getTopicMessageFactory().
+            visualization_msgs.Marker markerText = 
+              node.getTopicMessageFactory().
               newFromType(visualization_msgs.Marker._TYPE);
             markerText.setHeader(message.getHeader());
             TagPublisher.tagPoseToVisualizationText(tag, markerText);
