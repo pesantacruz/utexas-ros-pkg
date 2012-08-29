c2ee3c5e24220c1c53ae449779f65fbe1e76087f
liangfok@gmail.com <liangfok@gmail.com@aea2af56-0017-d0a0-7fdb-f656f34a69f4>
2012-08-29 15:55:13 +0000

    Cleaned up the code by removing unused imports

--- stacks/pharos/lightswitch_app/src/main/java/edu/utexas/ece/pharos/demo/homeAutomation/LightSwitchAppContinuous.java
+++ stacks/pharos/lightswitch_app/src/main/java/edu/utexas/ece/pharos/demo/homeAutomation/LightSwitchAppContinuous.java
@@ -5,7 +5,6 @@ import org.ros.namespace.GraphName;
 import org.ros.node.AbstractNodeMain;
 import org.ros.node.ConnectedNode;
 import org.ros.node.Node;
-import org.ros.node.NodeMain;
 import org.ros.node.topic.Publisher;
 import org.ros.node.topic.Subscriber;
 
