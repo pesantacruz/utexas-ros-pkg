f54279ad52990ea50ec874e8ec111d9930501c39
piyushk <piyushk@aea2af56-0017-d0a0-7fdb-f656f34a69f4>
2012-09-07 21:28:56 +0000

    added a qt based person controller package

---
+++ branches/experimental/stacks/bwi/bwi_person_controller/src/main_window.cpp
@@ -0,0 +1,114 @@
+/**
+ * @file /src/main_window.cpp
+ *
+ * @brief Implementation for the qt gui.
+ *
+ * @date February 2011
+ **/
+/*****************************************************************************
+ ** Includes
+ *****************************************************************************/
+
+#include <QtGui>
+#include <QMessageBox>
+#include <iostream>
+#include "../include/bwi_person_controller/main_window.hpp"
+
+/*****************************************************************************
+ ** Namespaces
+ *****************************************************************************/
+
+namespace bwi_person_controller {
+
+  using namespace Qt;
+
+  /*****************************************************************************
+   ** Implementation [MainWindow]
+   *****************************************************************************/
+
+  MainWindow::MainWindow(int argc, char** argv, QWidget *parent)
+    : QMainWindow(parent), qnode(argc,argv), current_speed(0.75), current_angular_speed(0.75)
+  {
+    ui.setupUi(this); // Calling this incidentally connects all ui's triggers to on_...() callbacks in this class.
+    setWindowIcon(QIcon(":/images/icon.png"));
+    QObject::connect(&qnode, SIGNAL(rosShutdown()), this, SLOT(close()));
+
+    move(0,0,*(ui.stopButton));
+  }
+
+  MainWindow::~MainWindow() {}
+
+  bool MainWindow::navigate(double x, double y, std::string level, std::string& error) {
+    // perhaps set some sort of feedback callback here?
+    return qnode.navigate(x,y,level,error);
+  }
+
+  void MainWindow::move(double x, double theta, QToolButton &button) {
+    ui.leftButton->setDown(false);
+    ui.rightButton->setDown(false);
+    ui.forwardButton->setDown(false);
+    ui.backwardButton->setDown(false);
+    ui.stopButton->setDown(false);
+    button.setDown(true);
+    qnode.move(x, theta);
+  }
+  
+  /*****************************************************************************
+   ** Implementation [Slots]
+   *****************************************************************************/
+  void MainWindow::on_leftButton_clicked() {
+    move(0, current_angular_speed, *(ui.leftButton));
+  }
+  void MainWindow::on_rightButton_clicked() {
+    move(0, -current_angular_speed, *(ui.rightButton));
+  }
+  void MainWindow::on_forwardButton_clicked() {
+    move(current_speed, 0, *(ui.forwardButton));
+  }
+  void MainWindow::on_backwardButton_clicked() {
+    move(-current_speed, 0, *(ui.backwardButton));
+  }
+  void MainWindow::on_stopButton_clicked() {
+    move(0, 0, *(ui.stopButton));
+  }
+  void MainWindow::on_danaButton_clicked() {
+    navigate(-5, 5, 
+    std::cout << "dana" << std::endl;
+  }
+  void MainWindow::on_piyushButton_clicked() {
+    std::cout << "piyush" << std::endl;
+  }
+  void MainWindow::on_samButton_clicked() {
+    std::cout << "sam" << std::endl;
+  }
+  void MainWindow::on_ensButton_clicked() {
+    std::cout << "ens" << std::endl;
+  }
+
+  void MainWindow::closeEvent(QCloseEvent *event)
+  {
+    QMainWindow::closeEvent(event);
+  }
+
+  void MainWindow::keyPressEvent(QKeyEvent *event) {
+    switch(event->key()) {
+      case Qt::Key_I:
+        on_forwardButton_clicked();
+        break;
+      case Qt::Key_K:
+        on_stopButton_clicked();
+        break;
+      case Qt::Key_Comma:
+        on_backwardButton_clicked();
+        break;
+      case Qt::Key_J:
+        on_leftButton_clicked();
+        break;
+      case Qt::Key_L:
+        on_rightButton_clicked();
+        break;
+    }
+  }
+
+}  // namespace bwi_person_controller
+
