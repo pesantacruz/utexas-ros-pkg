7b875d8a23adb43ebc75b8ec70ce1b39dd4a1de0
piyushk <piyushk@aea2af56-0017-d0a0-7fdb-f656f34a69f4>
2012-11-12 20:24:01 +0000

    improved auto-detect ROS_IP script

--- stacks/bwi/bwi_multimaster/data/setup.bash
+++ stacks/bwi/bwi_multimaster/data/setup.bash
@@ -1,4 +1,3 @@
 bwi_multimaster_location=`rospack find bwi_multimaster`
-export ROS_IP=`${bwi_multimaster_location}/scripts/get_addr.py wlan0`
+export ROS_IP=`${bwi_multimaster_location}/scripts/get_addr.py`
 export ROS_MASTER_URI=http://${ROS_IP}:11311
-
