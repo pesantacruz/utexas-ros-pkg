05212e6488e02b2956befdce68a613c960a5a947
piyushk <piyushk@aea2af56-0017-d0a0-7fdb-f656f34a69f4>
2012-11-12 20:24:01 +0000

    improved auto-detect ROS_IP script

--- branches/experimental/stacks/bwi/bwi_multimaster/scripts/get_addr.py
+++ branches/experimental/stacks/bwi/bwi_multimaster/scripts/get_addr.py
@@ -9,21 +9,22 @@ import sys
 import netifaces
 
 if len(sys.argv) != 2:
-    print >> sys.stderr, "ROS: Incorrect get_addr.py usage. USAGE get_addr.py <interface>. <interface> should be wlan0, eth0 etc."
-    print >> sys.stderr, "ROS: Using eth0 as the default interface"
-    interface = 'eth0'
+    preferred_interfaces = ['eth','wlan']
 else:
-    interface = sys.argv[1]
+    preferred_interfaces = [sys.argv[1]]
 
-for inf in netifaces.interfaces():
-    if inf.startswith(interface):
-        addrs = netifaces.ifaddresses(inf)
-        if not netifaces.AF_INET in addrs:
-            continue
-        else:
-            print addrs[netifaces.AF_INET][0]['addr']
-            sys.exit(0)
+sorted_interfaces = sorted(netifaces.interfaces())
 
-print >> sys.stderr, "ROS: failed to determine IP address for interface %s. You won't be able to connect to this machine externally."%interface
+for pref in preferred_interfaces:
+    for inf in netifaces.interfaces():
+        if inf.startswith(pref):
+            addrs = netifaces.ifaddresses(inf)
+            if not netifaces.AF_INET in addrs:
+                continue
+            else:
+                print addrs[netifaces.AF_INET][0]['addr']
+                sys.exit(0)
+
+print >> sys.stderr, "ROS: failed to determine IP address for interfaces %s. You won't be able to connect to this machine externally."%str(preferred_interfaces)
 print "127.0.0.1" #bind to loopback for now
 sys.exit(1)
