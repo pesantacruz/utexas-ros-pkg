56fe745f50bd6ade7b8e2bf2b38470953e535914
piyushk <piyushk@aea2af56-0017-d0a0-7fdb-f656f34a69f4>
2012-11-12 20:24:53 +0000

    updated install script to install system/local packages

--- branches/experimental/stacks/bwi/bwi_multimaster/scripts/setup_code_locally.sh
+++ branches/experimental/stacks/bwi/bwi_multimaster/scripts/setup_code_locally.sh
@@ -4,7 +4,7 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update
-sudo apt-get upgrade --yes && sudo apt-get install ros-fuerte-desktop-full python-rospkg python-rosdep python-rosinstall python-rosrelease ros-fuerte-camera-umd ros-fuerte-qt-ros ros-fuerte-turtlebot ros-fuerte-brown-remotelab python-netifaces vim subversion git mercurial openssh-server --yes
+sudo apt-get upgrade --yes && sudo apt-get install ros-fuerte-desktop-full python-rospkg python-rosdep python-rosinstall python-rosrelease ros-fuerte-camera-umd ros-fuerte-qt-ros ros-fuerte-turtlebot ros-fuerte-brown-remotelab ros-fuerte-freenect-stack ros-fuerte-zeroconf-avahi-suite python-netifaces vim subversion git mercurial openssh-server --yes
 
 #ros code built from scratch
 cd
@@ -14,9 +14,8 @@ rosws init
 rosws merge /opt/ros/fuerte/.rosinstall -y
 rosws merge http://utexas-ros-pkg.googlecode.com/svn/trunk/rosinstall/experimental.rosinstall -y
 rosws merge http://utexas-ros-pkg.googlecode.com/svn/trunk/rosinstall/segway_rmp.rosinstall -y
-rosws merge https://raw.github.com/robotics-in-concert/rocon_multimaster/master/rocon_multimaster.rosinstall -y
+rosws merge https://github.com/robotics-in-concert/rocon_multimaster/blob/master/rocon_multimaster_fuerte.rosinstall -y
 rosws update
 echo "source ~/ros/setup.bash" >> ~/.bashrc
-source ~/.bashrc
-rosmake rocon_gateway rocon_gateway_hub segway_rmp segbot_run segbot_navigation
+source ~/.bashrc && rosmake rocon_gateway rocon_gateway_hub segway_rmp segbot_run segbot_navigation
 echo "source \`rospack find bwi_multimaster\`/data/setup.bash" >> ~/.bashrc
