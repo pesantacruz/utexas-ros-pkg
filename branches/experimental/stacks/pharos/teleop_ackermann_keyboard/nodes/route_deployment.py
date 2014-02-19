06d817e93b296ee9218688c366ebeec4d8ac9dba
pedro.santacruz@gmail.com <pedro.santacruz@gmail.com@aea2af56-0017-d0a0-7fdb-f656f34a69f4>
2014-02-19 21:42:13 +0000

    Updated/debugged route_deployment

--- branches/experimental/stacks/pharos/teleop_ackermann_keyboard/nodes/route_deployment.py
+++ branches/experimental/stacks/pharos/teleop_ackermann_keyboard/nodes/route_deployment.py
@@ -10,10 +10,13 @@ turn = 20
 
 def moveForward(seconds):
 	start = time.time()
+	print start
 	time.clock()
 	elapsed = 0
-	while elapsed < seconds
+	while elapsed < seconds:
+		print"Moving Forward"
 		elapsed = time.time() - start
+		ackermannDriveMsg = AckermannDriveMsg()
 		ackermannDriveMsg.speed = 1*speed
 		ackermannDriveMsg.steering_angle = 0*turn 
 		rospy.loginfo("Publishing speed = %f, steering = %f", ackermannDriveMsg.speed, ackermannDriveMsg.steering_angle)		
@@ -23,8 +26,10 @@ def turnRight(seconds):
 	start = time.time()
 	time.clock()
 	elapsed = 0
-	while elapsed < seconds
+	while elapsed < seconds:
+		print"Turning Right"
 		elapsed = time.time() - start
+		ackermannDriveMsg = AckermannDriveMsg()
 		ackermannDriveMsg.speed = 1*speed
 		ackermannDriveMsg.steering_angle = 1*turn 
 		rospy.loginfo("Publishing speed = %f, steering = %f", ackermannDriveMsg.speed, ackermannDriveMsg.steering_angle)		
@@ -34,8 +39,10 @@ def turnLeft(seconds):
 	start = time.time()
 	time.clock()
 	elapsed = 0
-	while elapsed < seconds
+	while elapsed < seconds:
+		print"Turning Left"
 		elapsed = time.time() - start
+		ackermannDriveMsg = AckermannDriveMsg()
 		ackermannDriveMsg.speed = 1*speed
 		ackermannDriveMsg.steering_angle = -1*turn 
 		rospy.loginfo("Publishing speed = %f, steering = %f", ackermannDriveMsg.speed, ackermannDriveMsg.steering_angle)		
