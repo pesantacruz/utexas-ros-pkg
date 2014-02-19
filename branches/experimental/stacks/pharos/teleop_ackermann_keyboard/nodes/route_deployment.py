c95281a91798c9cd7cf5aa7bcecc58ee0d8bd122
pedro.santacruz@gmail.com <pedro.santacruz@gmail.com@aea2af56-0017-d0a0-7fdb-f656f34a69f4>
2014-02-19 00:18:44 +0000

    Added route_deployment.py to the teleop_ackermann_keyboard package for testing

---
+++ branches/experimental/stacks/pharos/teleop_ackermann_keyboard/nodes/route_deployment.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+import rospy
+import time
+from std_msgs.msg import String
+from traxxas_node.msg import AckermannDriveMsg
+import sys, select, termios, tty
+
+speed = .5
+turn = 20
+
+def moveForward(seconds):
+	start = time.time()
+	time.clock()
+	elapsed = 0
+	while elapsed < seconds
+		elapsed = time.time() - start
+		ackermannDriveMsg.speed = 1*speed
+		ackermannDriveMsg.steering_angle = 0*turn 
+		rospy.loginfo("Publishing speed = %f, steering = %f", ackermannDriveMsg.speed, ackermannDriveMsg.steering_angle)		
+		pub.publish(ackermannDriveMsg)
+
+def turnRight(seconds):
+	start = time.time()
+	time.clock()
+	elapsed = 0
+	while elapsed < seconds
+		elapsed = time.time() - start
+		ackermannDriveMsg.speed = 1*speed
+		ackermannDriveMsg.steering_angle = 1*turn 
+		rospy.loginfo("Publishing speed = %f, steering = %f", ackermannDriveMsg.speed, ackermannDriveMsg.steering_angle)		
+		pub.publish(ackermannDriveMsg)
+
+def turnLeft(seconds):
+	start = time.time()
+	time.clock()
+	elapsed = 0
+	while elapsed < seconds
+		elapsed = time.time() - start
+		ackermannDriveMsg.speed = 1*speed
+		ackermannDriveMsg.steering_angle = -1*turn 
+		rospy.loginfo("Publishing speed = %f, steering = %f", ackermannDriveMsg.speed, ackermannDriveMsg.steering_angle)		
+		pub.publish(ackermannDriveMsg)
+
+
+
+if __name__=="__main__":
+
+   	settings = termios.tcgetattr(sys.stdin)
+
+	
+	pub = rospy.Publisher('/traxxas_node/ackermann_drive', AckermannDriveMsg)
+	rospy.init_node('route_deployment')
+
+	
+	status = 0
+
+	try:
+			print"Begin Moving"
+			moveForward(10)
+			r = rospy.Rate(10)
+			r.sleep()
+			turnRight(3)
+			moveForward(10)
+			r.sleep()
+			
+	except:
+		print e
+
+	finally:
+		ackermannDriveMsg = AckermannDriveMsg()
+		#twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
+		#twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+		pub.publish(AckermannDriveMsg)
+
+    		termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+
+
