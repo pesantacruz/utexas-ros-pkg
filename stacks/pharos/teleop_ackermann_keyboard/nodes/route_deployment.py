2c59e0a6828a3b4035530d2496f39ec3e826c585
pedro.santacruz@gmail.com <pedro.santacruz@gmail.com@aea2af56-0017-d0a0-7fdb-f656f34a69f4>
2014-02-20 01:08:39 +0000

    route_deployment.py debug

--- stacks/pharos/teleop_ackermann_keyboard/nodes/route_deployment.py
+++ stacks/pharos/teleop_ackermann_keyboard/nodes/route_deployment.py
@@ -6,14 +6,14 @@ from traxxas_node.msg import AckermannDriveMsg
 import sys, select, termios, tty
 
 speed = .5
-turn = 20
+turn = 70
 
 def moveForward(seconds):
 	start = time.time()
 	print start
 	time.clock()
 	elapsed = 0
-	while elapsed < seconds:
+	while elapsed < seconds and not rospy.is_shutdown():
 		print"Moving Forward"
 		elapsed = time.time() - start
 		ackermannDriveMsg = AckermannDriveMsg()
@@ -26,12 +26,12 @@ def turnRight(seconds):
 	start = time.time()
 	time.clock()
 	elapsed = 0
-	while elapsed < seconds:
+	while elapsed < seconds and not rospy.is_shutdown():
 		print"Turning Right"
 		elapsed = time.time() - start
 		ackermannDriveMsg = AckermannDriveMsg()
 		ackermannDriveMsg.speed = 1*speed
-		ackermannDriveMsg.steering_angle = 1*turn 
+		ackermannDriveMsg.steering_angle = -1*turn 
 		rospy.loginfo("Publishing speed = %f, steering = %f", ackermannDriveMsg.speed, ackermannDriveMsg.steering_angle)		
 		pub.publish(ackermannDriveMsg)
 
@@ -39,12 +39,12 @@ def turnLeft(seconds):
 	start = time.time()
 	time.clock()
 	elapsed = 0
-	while elapsed < seconds:
+	while elapsed < seconds and not rospy.is_shutdown():
 		print"Turning Left"
 		elapsed = time.time() - start
 		ackermannDriveMsg = AckermannDriveMsg()
 		ackermannDriveMsg.speed = 1*speed
-		ackermannDriveMsg.steering_angle = -1*turn 
+		ackermannDriveMsg.steering_angle = 1*turn 
 		rospy.loginfo("Publishing speed = %f, steering = %f", ackermannDriveMsg.speed, ackermannDriveMsg.steering_angle)		
 		pub.publish(ackermannDriveMsg)
 
@@ -53,7 +53,11 @@ def turnLeft(seconds):
 if __name__=="__main__":
 
    	settings = termios.tcgetattr(sys.stdin)
-
+	
+	if(len(sys.argv) > 1):
+		second = int(sys.argv[1])
+	else:
+		second = 2
 	
 	pub = rospy.Publisher('/traxxas_node/ackermann_drive', AckermannDriveMsg)
 	rospy.init_node('route_deployment')
@@ -62,13 +66,15 @@ if __name__=="__main__":
 	status = 0
 
 	try:
+		while not rospy.is_shutdown():
 			print"Begin Moving"
-			moveForward(10)
-			r = rospy.Rate(10)
-			r.sleep()
+			moveForward(second)
 			turnRight(3)
-			moveForward(10)
-			r.sleep()
+			moveForward(second)
+			turnRight(3)
+			moveForward(second)
+			turnRight(3)
+			moveForward(second)
 			
 	except:
 		print e
