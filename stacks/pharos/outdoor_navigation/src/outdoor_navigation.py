dbfce10b3deb2df2d4496ec043309885ffb1a7b1
pedro.santacruz@gmail.com <pedro.santacruz@gmail.com@aea2af56-0017-d0a0-7fdb-f656f34a69f4>
2014-03-28 20:54:13 +0000

    Outdoor navigation update

--- stacks/pharos/outdoor_navigation/src/outdoor_navigation.py
+++ stacks/pharos/outdoor_navigation/src/outdoor_navigation.py
@@ -17,6 +17,9 @@ close_enough = 2.5
 # specifies the navigation component's cycle time
 nav_cycle_period = 200
 
+final_lat = 0
+final_lon = 0
+
 # Initialize GPS and compass readings
 cur_lon = 0
 cur_lat = 0
@@ -30,38 +33,34 @@ roll = 0
 
 # gets robot's current longitude and latitude from gps msg
 def get_gps(data):
+	a = data.latitude
+	b = data.longitude
+	
+ 	print rospy.get_caller_id(),'Received Coordinates - Latitude: ',data.latitude,' Longitude: ',data.longitude
 
-	print rospy.get_caller_id(),'Received Coordinates - Longitude: ',data.longitude,' Latitude: ',data.latitude
-
+	check(a, b)
 
 
 # gets robot's current heading, pitch, and roll, from compass msg
 def get_compass(data):
+	heading = data.heading
+	pitch = data.pitch
+	roll = data.roll
 
-	print rospy.get_caller_id(),'Received Compass Info - Heading: ',data.heading
+	# print rospy.get_caller_id(),'Received Compass Info - Heading: ',data.heading
 
 
 
-class Navigate(Thread):
-	"""Thread to run navigation computations and publish to the traxxas_node AckermannDriveMsg for movement"""
-	
-	# constructor
-	def __init__(self, dest_longitude, dest_latitude):
-		# call Thread superclass constructor
-		Thread.__init__(self, name="Navigate")
-		
-		# set class variables
-		self.dest_longitude = dest_longitude
-		self.dest_latitude = dest_latitude
+def check(a, b):
+	print 'Current Location:', a, b
+	print 'Destination:', final_lat, final_lon
 
-	# fuction called by Thread.start() to start navigation
-	def run(self):
-		print "Thread Navigate Starting."
-		print 'Navigating to (', dest_longitude, ',', dest_latitude, ')'
+	distance = get_distance(a, b, final_lat, final_lon)
 
+	print "Distance to destination:", distance
 
 # calculates distance from location A (lon_A, lat_A) to B (lon_B, lat_B)
-def get_distance(lon_A, lat_A, lon_B, lat_B):
+def get_distance(lat_A, lon_A, lat_B, lon_B):
 	# uses "Haversine Formula"
 
 	# approx radius of earth in kilometers
@@ -88,95 +87,32 @@ def get_distance(lon_A, lat_A, lon_B, lat_B):
 # checks if robot's current gps location is close enough to destination
 def there_yet(longitude, latitude):
 	
-	(cur_lon, cur_lat) = get_gps()
-
 	distance = get_distance(cur_lon, cur_lat, longitude, latitude)
 
-	return distance
 
-	# if distance < close_enough:
-	# 	return True
-	# else:
-	# 	return False
+	if distance < close_enough:
+		return True
+	else:
+		return False
 
-	# pass
-
-
-
-# def do_next_move():
-# 	# TODO
-# 	pass
-
-
-
-# def navigate(longitude, latitude, speed):
-# 	done = False
-# 	error = False
-
-# 	while(not done and not error):
-# 		(cur_lon, cur_lat) = get_gps()
-# 		# check if current coordinates are valid
-
-# 		cur_heading = get_compass()[0]
-# 		# check if current heading is valid
-
-# 		done = do_next_move(cur_lon, cur_lat, longitude, latitude, speed)
-
-# 		if not done:
-# 			time.sleep(nav_cycle_period / 1000)
-
-# 	pass
 
 
 
 if __name__ == '__main__':
 	rospy.init_node('outdoor_navigation', anonymous=True)
 
-
-	# parse arguments
-	# TODO better arugments with argparse library
-	# if len(sys.argv) < 3:
-	# 	print 'No arguments provided.\n', 'Usage: navigate.py [longitude] [latitude]'
-	# 	sys.exit(1)
-
+	final_lat = float(sys.argv[1])
+	final_lon = float(sys.argv[2])
+	
 	rospy.Subscriber("gps/measurement", GPSMsg, get_gps)
 
 	rospy.Subscriber("compass/measurement", CompassMsg, get_compass)
 
 
-	longitude = 0
-	latitude = 0
-	# set location to navigate
-	# longitude = sys.argv[0]
-	# latitude = sys.argv[1]
-	# speed = 1;
 
-	# final_lon = float(30)
-	# final_lat = float(-98)
-
-	# (cur_lon, cur_lat) = get_gps()
-	# (heading, pitch, roll) = get_compass()
-
-	# distance = there_yet(final_lon, final_lat)
-
-	# print 'Distance to desired location: ', distance
-
-	# print 'Heading = ', heading
-
-	navigate_thread = Navigate(longitude, latitude)
-	navigate_thread.start()
+	
+	
 
 	rospy.spin()
 
-	# subscribe to GPS message data
-	# subscribe to Compass message data
-	# TODO
-	'''need to setup get_gps and get_compass function -- ask pedro for help''' 
-
-
-
-	# # call navigate function
-	# if navigate(latitude, longitude, speed) == True:
-	# 	print 'Success, now at location:', get_location()
-	# else:
-	# 	print 'Failure, now at location:', get_locatoin()
+	
